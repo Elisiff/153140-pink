6056e20fcc538c10e1d14cd840e5876888244a04
Anastasiya Vasina <nast791@mail.ru>
2017-03-28 14:50:44 +0300

    Первые попытки написания скрипта

--- js/script.js
+++ js/script.js
@@ -1,53 +1,90 @@
-var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle');
-var popFailure = document.querySelector('.popup-failure');
-var reviewsSlideOne = document.querySelector(".reviews__slide-1");
-var reviewsSlideTwo = document.querySelector(".reviews__slide-2");
-var reviewsSlideThree = document.querySelector(".reviews__slide-3");
-var reviewsArrowRight = document.querySelector(".reviews__slider-control-desktop--right");
-var reviewsArrowLeft = document.querySelector(".reviews__slider-control-desktop--left");
-var reviewsBtnOne = document.getElementById('reviews-btn-1');
-var reviewsBtnTwo = document.getElementById('reviews-btn-2');
-var reviewsBtnThree = document.getElementById('reviews-btn-3');
+let navMain = document.querySelector(".main-nav");
+let navToggle = document.querySelector(".main-nav__toggle");
+let reviewsArrowRight = document.querySelector(".reviews__slider-control-desktop--right");
+let reviewsArrowLeft = document.querySelector(".reviews__slider-control-desktop--left");
+let reviewsBtnOne = document.getElementById("reviews-btn-1");
+let reviewsBtnTwo = document.getElementById("reviews-btn-2");
+let reviewsBtnThree = document.getElementById("reviews-btn-3");
+let formSubmit = document.querySelector(".journey-form__submit-btn");
+let acqInput = document.querySelector(".journey-form__acquaintance-input--necessarily");
+let popFailure = document.querySelector(".popup-failure");
+let popSuccess = document.querySelector(".popup-success");
+let popOverlay = document.querySelector(".popup-overlay");
+let popSuccessBtn = document.querySelector ("popup-success__btn");
+let popFailureBtn = document.querySelector ("popup-failure__btn");
 
-var formSubmit = document.querySelector('.journey-form__submit-btn');
-var acqInput = document.querySelector('.journey-form__acquaintance-input');
+navMain.classList.remove("main-nav--nojs");
 
-navMain.classList.remove('main-nav--nojs');
-
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
+navToggle.addEventListener("click", function() {
+  if (navMain.classList.contains("main-nav--closed")) {
+    navMain.classList.remove("main-nav--closed");
+    navMain.classList.add("main-nav--opened");
   } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
+    navMain.classList.add("main-nav--closed");
+    navMain.classList.remove("main-nav--opened");
   }
 });
 
 // Слайдер с отзывами на десктопной версии
-// reviewsArrowRight.addEventListener("click", function(event) {
-//   event.preventDefault();
-//   if (reviewsBtnOne.hasAttribute('checked')) {
-//     reviewsBtnOne.removeAttribute('checked');
-//     reviewsBtnTwo.setAttribute('checked');
-//   } else if (reviewsBtnTwo.hasAttribute('checked')) {
-//     reviewsBtnTwo.removeAttribute('checked');
-//     reviewsBtnThree.setAttribute('checked');
-//   } else if (reviewsBtnThree.hasAttribute('checked')) {
-//     reviewsBtnThree.removeAttribute('checked');
-//     reviewsBtnOne.setAttribute('checked');
-//   }
-// });
+if (reviewsArrowRight !== null) {
+  reviewsArrowRight.addEventListener("click", function(event) {
+    event.preventDefault();
+    if (reviewsBtnOne.checked === true) {
+      reviewsBtnOne.checked = false;
+      reviewsBtnTwo.checked = true;
+    } else if (reviewsBtnTwo.checked === true) {
+      reviewsBtnTwo.checked = false;
+      reviewsBtnThree.checked = true;
+    } else if (reviewsBtnThree.checked === true) {
+      reviewsBtnThree.checked = false;
+      reviewsBtnOne.checked = true;
+    }
+  });
+}
 
-// reviewsArrowLeft.addEventListener("click", function(event) {
+if (reviewsArrowLeft !== null) {
+  reviewsArrowLeft.addEventListener("click", function(event) {
+    event.preventDefault();
+    if (reviewsBtnOne.checked === true) {
+      reviewsBtnOne.checked = false;
+      reviewsBtnThree.checked = true;
+    } else if (reviewsBtnTwo.checked === true) {
+      reviewsBtnTwo.checked = false;
+      reviewsBtnOne.checked = true;
+    } else if (reviewsBtnThree.checked === true) {
+      reviewsBtnThree.checked = false;
+      reviewsBtnTwo.checked = true;
+    }
+  });
+}
 
-// });
+//Запуск модальных окон
+if (formSubmit !== null) {
+  formSubmit.addEventListener("click", function() {
+    if (acqInput.value === "") {
+      popFailure.classList.remove("popup-failure--closed");
+      popFailure.classList.add("popup-failure--show");
+      popOverlay.classList.add("popup-overlay--show");
+    } else (acqInput.value !== "") {
+      popSuccess.classList.remove("popup-success--closed");
+      popSuccess.classList.add("popup-success--show");
+      popOverlay.classList.add("popup-overlay--show");
+    }
+  });
+}
 
-// Запуск модальных окон
-// formSubmit.addEventListener('click', function() {
-//   if (document.acqInput.value == "") {
-//       popFailure.classList.remove('popup-failure');
-//       popFailure.classList.add('popup-failure--show');
-//       }
-// });
+popSuccessBtn.addEventListener("click", function() {
+  if (popSuccess.contains("popup-success--show")) {
+    popSuccess.classList.remove("popup-success--show");
+    popFailure.classList.add("popup-success--closed");
+    popOverlay.classList.remove("popup-overlay--show");
+  }
+});
+
+popFailureBtn.addEventListener("click", function()) {
+  if (popFailure.contains("popup-failure--show")) {
+    popSuccess.classList.remove("popup-failure--show");
+    popFailure.classList.add("popup-failure--closed");
+    popOverlay.classList.remove("popup-overlay--show");
+  }
+});
